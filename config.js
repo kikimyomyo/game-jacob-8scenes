9100df3ba2740f787b99159de82d14828c819373
kikimyomyo <request1004@gmail.com>
2026-05-09 11:31:00 +0900

    deploy: jacob-8scenes

---
+++ config.js
@@ -0,0 +1,6 @@
+window.GAME_CONFIG = {
+  game: "jacob-8scenes",
+  title: "야곱 8장면",
+  sheet_tab: "jacob-8scenes",
+  endpoint: "https://script.google.com/macros/s/AKfycbx3a96h-vRywYTCxOJxdn199uaFAELyfzy7LRIGeInFuG-ZhxsBh_AKso--Yw8PbYPH/exec",
+};
